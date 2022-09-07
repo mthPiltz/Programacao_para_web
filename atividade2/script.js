01e4b29110421b8d91357a8d957d8d517aafcd84
Piltz <matheus.piltz@ufms.br>
2022-09-07 15:23:32 -0400

    Primeiro commit

---
+++ atividade2/script.js
@@ -0,0 +1,9 @@
+function mudarCor() {
+    const div = document.getElementById("quadrado");
+    div.style["background-color"] = "red";
+}
+
+function voltarCor() {    
+    const div = document.getElementById("quadrado");
+    div.style["background-color"] = "gray";
+}
