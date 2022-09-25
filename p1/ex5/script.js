74ba04a99843aec2ee0b0b414c4e5b91d679b5c8
Piltz <matheus.piltz@ufms.br>
2022-09-25 09:21:24 -0400

    resolução do exercicio 5 da p1

---
+++ p1/ex5/script.js
@@ -0,0 +1,18 @@
+var c = document.getElementById("canvas");
+var ctx = c.getContext("2d");
+
+// //linha
+ctx.moveTo(124, 11);
+ctx.lineTo(146,55);
+ctx.lineTo(208,55);
+ctx.lineTo(153,90);
+ctx.lineTo(173,140);
+ctx.lineTo(124,100);
+ctx.lineTo(75,140);
+ctx.lineTo(95,90);
+ctx.lineTo(45,55);
+ctx.lineTo(102,55);
+ctx.lineTo(124,11);
+
+ctx.stroke();
+
