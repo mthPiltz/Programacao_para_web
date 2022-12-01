7d27ab468ddea5beda1c269fe5524f6d47e6ec4c
Piltz <matheus.piltz@ufms.br>
2022-11-30 21:55:27 -0400

    Configuação das rotas

--- angular/heros/src/app/app-routing.module.ts
+++ angular/heros/src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { TelaCadastroComponent } from './tela-cadastro/tela-cadastro.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LoginComponent},
+  { path: 'login', component: LoginComponent },
+  { path: 'cadastro', component: TelaCadastroComponent }    
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
--- angular/heros/src/app/app.module.ts
+++ angular/heros/src/app/app.module.ts
@@ -5,16 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { LoginComponent } from './login/login.component';
+import { TelaCadastroComponent } from './tela-cadastro/tela-cadastro.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    LoginComponent
+    LoginComponent,
+    TelaCadastroComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
